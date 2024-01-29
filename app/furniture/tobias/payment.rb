232c4f665263ebe622f30601ef84a00134d5031f
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-01-29 15:40:44 -0800

    🥗🧹 `Tobias`: Define `Payment` relationships and attributes
    This is not super important to the actual feature, but it does mean that
    the `Payment` has clearly defined relationships, with tests that confirm
    the relationship.
    
    It also adds a spec to check that the `#amount` value operates as a
    monetized value; which doesn't seem like it does that much beyond giving
    us an injection point for layering in additional details.

--- app/furniture/tobias/payment.rb
+++ app/furniture/tobias/payment.rb
@@ -2,7 +2,7 @@ class Tobias
   class Payment < ApplicationRecord
     self.table_name = "tobias_payments"
 
-    belongs_to :payout
+    belongs_to :payout, inverse_of: :payments
 
     monetize :amount_cents
   end
