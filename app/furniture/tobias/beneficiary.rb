264a1a732c97869acbd629fd8de327596aa7123f
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-01-29 16:03:59 -0800

    🥗🧹 `Tobias`: `Beneficiary` has defined associations

--- app/furniture/tobias/beneficiary.rb
+++ app/furniture/tobias/beneficiary.rb
@@ -2,8 +2,9 @@ class Tobias
   class Beneficiary < ApplicationRecord
     self.table_name = "tobias_beneficiaries"
 
-    belongs_to :trust
+    belongs_to :trust, inverse_of: :beneficiaries
 
-    has_many :payments
+    has_many :payments, inverse_of: :beneficiary
+    has_many :payouts, through: :payments
   end
 end
