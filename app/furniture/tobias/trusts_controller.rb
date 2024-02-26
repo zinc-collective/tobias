55affc07a77c10ebf6a781c5109274773bdf7ff5
chloeinnvaer <134097748+chloeinnvaer@users.noreply.github.com>
2024-02-26 15:59:45 -0800

    ✨ `Tobias`: Flesh out the pathway to `Trust#show`
    Next steps might include:
    create "New Payout" link
    
    Co-Authored-By: Zee <50284+zspencer@users.noreply.github.com>
    Co-Authored-By: JuanCarlosTC <153764163+juancarlostc@users.noreply.github.com>

--- app/furniture/tobias/trusts_controller.rb
+++ app/furniture/tobias/trusts_controller.rb
@@ -1,8 +1,16 @@
 class Tobias
   class TrustsController < FurnitureController
+    expose :trust, model: Trust
 
     def show
+      authorize trust
     end
 
+  end
+  class TrustPolicy < ApplicationPolicy
+      def show?
+        true
+      end
+    alias_method :entry, :object
   end
 end
