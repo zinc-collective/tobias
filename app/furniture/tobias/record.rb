d7af1605a5146caf2f07d716ca4401903e5c0602
Zee Spencer <50284+zspencer@users.noreply.github.com>
2024-03-25 14:26:45 -0700

    🧹 Manually overriding`#model_name` is a bit odd
    Instead, we use the mixin as a kind of
    "nod-to-the-odd"

--- app/furniture/tobias/record.rb
+++ app/furniture/tobias/record.rb
@@ -1,9 +1,7 @@
 class Tobias
   class Record < ApplicationRecord
     self.abstract_class = true
+    extend StripsNamespaceFromModelName
 
-    def self.model_name
-      @_model_name ||= ActiveModel::Name.new(self, ::Tobias)
-    end
   end
 end
